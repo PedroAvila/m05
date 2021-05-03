f6b82612047a1ba933d7ee88c7456e948277505a
PedroAvila <minimainor_austin@hotmail.com>
2021-05-03 11:03:40 -0500

    Características de la sintaxis Razor

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,6 +11,8 @@ namespace M05.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            IEnumerable<string> dias = new string[] { "Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado" };
+            ViewBag.dias = dias;
             return View();
         }
     }
